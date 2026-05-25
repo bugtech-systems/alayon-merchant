4fa3fbb89d0a1927a17597f4e6c402a52aac53a6
I am root <bugtechsystems@gmail.com>
2026-05-25 11:52:22 +0800

    Fix things up

--- apps/web/components/sidebar/nav-user.tsx
+++ apps/web/components/sidebar/nav-user.tsx
@@ -32,7 +32,7 @@ authLogout()
 
 
 let avatar = "https://robohash.org/" +
-            user.id +
+            user?.id +
             "?size=200x200&set=set1&bgset=bg1"
 
   return (
@@ -45,12 +45,12 @@ let avatar = "https://robohash.org/" +
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg ">
-                <AvatarImage src={avatar} alt={user.first_name} />
-                <AvatarFallback className="rounded-lg">{getInitials(user.first_name)}</AvatarFallback>
+                <AvatarImage src={avatar} alt={user?.first_name} />
+                <AvatarFallback className="rounded-lg">{getInitials(user?.first_name)}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.first_name}</span>
-                <span className="truncate text-muted-foreground text-xs">{user.email}</span>
+                <span className="truncate font-medium">{user?.first_name}</span>
+                <span className="truncate text-muted-foreground text-xs">{user?.email}</span>
               </div>
               <EllipsisVertical className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -64,12 +64,12 @@ let avatar = "https://robohash.org/" +
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={avatar} alt={user.first_name} />
-                  <AvatarFallback className="rounded-lg">{getInitials(user.first_name)}</AvatarFallback>
+                  <AvatarImage src={avatar} alt={user?.first_name} />
+                  <AvatarFallback className="rounded-lg">{getInitials(user?.first_name)}</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.first_name}</span>
-                  <span className="truncate text-muted-foreground text-xs">{user.email}</span>
+                  <span className="truncate font-medium">{user?.first_name}</span>
+                  <span className="truncate text-muted-foreground text-xs">{user?.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
