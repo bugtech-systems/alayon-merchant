3edc954256baa2e1ccaef343f26ac3ed022ebf0a
I am root <bugtechsystems@gmail.com>
2026-07-10 12:16:45 +0800

    Fix this

--- apps/web/app/pos/_components/ReceiptBuilder.tsx
+++ apps/web/app/pos/_components/ReceiptBuilder.tsx
@@ -20,7 +20,7 @@ export class ReceiptBuilder {
   PAPER_WIDTH = 32;
 
   formatCurrency(amount) {
-    return `₱${Number(amount).toFixed(2)}`;
+    return `Php: ${Number(amount).toFixed(2)}`;
   }
 
   formatDate(dateString) {
@@ -137,7 +137,7 @@ console.log(order.metadata, order, 'HOOORD')
       .bold(true)
       .text(this.fitText('ORDER DETAILS'))
       .bold(false)
-      .text(this.fitText(`Order #: ${order.display_id || order.id || 'N/A'} || ${order?.metadata?.table_ids?.[0]}`))
+      .text(this.fitText(`Order #: ${order.display_id || order.id || 'N/A'} || ${order?.metadata?.table_ids?.[0] ? order?.metadata?.table_ids?.[0] : ''}`))
       .text(this.fitText(`Date: ${this.formatDate(order.created_at)}`))
       .text(this.fitText(`Status: ${order.status?.toUpperCase() || 'COMPLETED'}`))
       .newline(2);
